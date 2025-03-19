c8b226ca203689548dbd122a9552f7a38af2e3ed
RajeevniUmapathisivam <rajeevniumapathisivam@gmail.com>
2025-03-19 21:16:36 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,11 @@
+//JSON
+//{key:value}
+let student ={regno:'2021/ICT/01',name:'James',age:21,course:'IT',skills:['Java','c++','JS']}
+console.log(student)
+console.log("Name: "+student.name)
+
+let students =[
+{regno:'2021/ICT/01',name:'James',age:21,course:'IT'},
+{regno:'2021/ICT/02',name:'ravi',age:24,course:'IT'},
+{regno:'2021/ICT/03',name:'gaya',age:23,course:'IT'}]
+console.log(students)
