149561410e7b6c4da8bd308e06adf73186fb4a67
RajeevniUmapathisivam <rajeevniumapathisivam@gmail.com>
2025-03-19 21:13:29 +0530

    Add files via upload

---
+++ PushPop.js
@@ -0,0 +1,18 @@
+//Array operation
+//push and pop
+arr3 =['a','b','d']
+console.log(arr3)
+arr3.push('e')
+console.log(arr3)
+arr3.pop()
+console.log(arr3)
+
+//reverse the array using push and pop
+//a b c d ->d c b a 
+
+ let arr4 =['a','b','c','d']
+for(let i=0;i<arr4.length-1;i++)
+{
+	console.log(arr4.pop())
+}
+ 
