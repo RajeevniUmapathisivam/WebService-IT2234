3c51bfb9e28357618068a418db1d1d25b0c008ec
RajeevniUmapathisivam <rajeevniumapathisivam@gmail.com>
2025-04-06 18:12:52 +0530

    Add files via upload

---
+++ asynawait.js
@@ -0,0 +1,20 @@
+const fs =require('fs').promises;
+
+const readFile =async ()=>{
+    try {
+        const [data,data2] =await Promise.allSettled([
+            fs.readFile('file.txt','utf8'),
+            fs.readFile('data.txt','utf8')
+        ])
+        //await fs.readFile('file.txt','utf8')
+        console.log(data.value)
+        console.log(data2)
+        console.log(data.status)    //status -undefined
+        console.log(data2.status)
+    }catch(err){
+        
+        console.error(err);
+    }
+
+}
+readFile()
